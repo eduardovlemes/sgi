9bfa17245c451e9fcb52e235b093ec40eb04ab2c
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-11 22:29:35 -0300

    feat: validação de campos
    Adiciona função handleSubmit que valida campos e conduz para página de mapa

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,19 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  return(
-    <form>
+  function handleSubmit(event) {
+    try {
+      event.preventDefaul();
+      if (!email) {
+        alert("O preenchimento do campo Email é obrigatório.");
+        return;
+      } else if (!password) {
+        alert("O preenchimento do campo Senha é obrigatório.");
+        return;
+      }
+      event.target.checkValidity();
+      navigate("/mapa");
+    } catch (error) {
+      alert("Erro no preenchimento dos campos. Tente novamente.");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Email <input type="email" placeholder="exemplo@mail.com" required value={email} onChange={(event) => setEmail(event.target.value)}/></label>
-        <label>Senha <input type="password" required value={password} onChange={(event) => setPassword(event.target.value)}/></label>
-        <a>Cadastrar usuário</a>
+        <label>
+          Email<input
+            type="email"
+            placeholder="exemplo@mail.com"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          Senha<input
+            type="password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
         <div>
           <button>Acessar</button>
         </div>
       </div>
     </form>
-  )
+  );
 }
