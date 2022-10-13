dcfccc16480802bbbf93fa8d7748d1f3978c6182
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-13 19:54:18 -0300

    feat: cria o componente ListItems
    Cria o componente que apresenta os produtos cadastrados na tela, passando dados por props.

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -1,3 +1,22 @@
-export default function ListItems() {
-  return;
+export default function ListItems({
+  alt,
+  src,
+  name,
+  price,
+  description,
+  caterer,
+  group,
+}) {
+  return (
+    <li>
+      <img alt={alt} src={src} width={250}/>
+      <div>
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <p>{description}</p>
+        <p>{caterer}</p>
+        <p>{group}</p>
+      </div>
+    </li>
+  );
 }
