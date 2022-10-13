228627661d09bda0b4800fe2ac07a25e1e35920f
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-13 20:28:09 -0300

    feat: cria componente Header
    Cria o botões no cabeçalho que direciona para as páginas.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,27 @@
-export default function Header (){
-    return
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    if (confirm("Deseja sair?")) {
+      navigate("/");
+    }
+  }
+
+  return (
+    <header>
+      <nav>
+        <button onClick={() => navigate("/novaempresa")}>
+          CADASTRAR EMPRESA
+        </button>
+        <button onClick={() => navigate("/mapa")}>MAPA</button>
+        <button onClick={() => navigate("/novoitem")}>CADASTRAR PRODUTO</button>
+        <button onClick={() => navigate("/listaitems")}>
+          LISTA DE PRODUTOS
+        </button>
+        <button onClick={handleLogout}>SAIR</button>
+      </nav>
+    </header>
+  );
 }
