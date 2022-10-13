927aaeeecc7b63bf7cc811f19722eb19d96122a4
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-13 20:28:53 -0300

    feat: cria componente Footer
    Cria a estrutura com links no Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,33 @@
-export default function Footer (){
-    return
+export default function Footer() {
+  return (
+    <footer>
+      <div />
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/eduardovlemes/"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://pt-br.facebook.com/"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+          ></a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/"
+            target="_blank"
+          ></a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
