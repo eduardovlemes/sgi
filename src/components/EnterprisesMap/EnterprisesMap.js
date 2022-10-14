bc624209056c3aea6474b06921afc8638800a985
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-13 22:06:47 -0300

    feat: instala e cria componente mapa
    Instala biblioteca e cria componente de mapa

--- src/components/EnterprisesMap/EnterprisesMap.js
+++ src/components/EnterprisesMap/EnterprisesMap.js
@@ -1,3 +1,71 @@
-export default function EnterpriseMap (){
-    return
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+
+export default function EnterpriseMap() {
+  const [enterprises, setEnterprises] = useState([]);
+
+  useEffect(() => {
+    async function getEnterprises() {
+      await fetch("http://localhost:3001/enterprises")
+        .then((response) => response.json())
+        .then((dataEnterprisesServer) => {
+          setEnterprises(dataEnterprisesServer);
+        });
+    }
+    getEnterprises();
+  }, []);
+
+  return (
+    <div>
+      <h2>Empresas cadastradas</h2>
+      <MapContainer
+        center={[-13.747492, -53.470339]}
+        zoom={4}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {enterprises.map((enterprise) => (
+          <Marker position={[enterprise.latitude, enterprise.longitude]}>
+            <Popup>
+              <>
+                <p>
+                  <strong>Razão Social: </strong>
+                  {enterprise.corporateName}
+                </p>
+                <p>
+                  <strong>Nome Fantasia: </strong>
+                  {enterprise.tradeName}
+                </p>
+                <p>
+                  <strong>CNPJ: </strong>
+                  {enterprise.cnpj}
+                </p>
+                <p>
+                  <strong>Email: </strong>
+                  {enterprise.email}
+                </p>
+                <p>
+                  <strong>Endereço: </strong>
+                  {enterprise.street}, nº {enterprise.addressNumber} -{" "}
+                  {enterprise.district} - {enterprise.city} -{" "}
+                  {enterprise.postalcode}
+                </p>
+
+                {enterprise.addressCompl !== "" ? (
+                  <p>
+                    <strong>Compl.: </strong>
+                    {enterprise.addressCompl}
+                  </p>
+                ) : null}
+              </>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
